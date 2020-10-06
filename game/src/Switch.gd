c049b6eb2b3839e9624da59a2dc6a32f4590efb6
lpavezb <lukas.pavez@ug.uchile.cl>
2020-10-06 03:51:23 -0300

    add animations for panel buttons and switch

---
+++ game/src/Switch.gd
@@ -0,0 +1,38 @@
+extends StaticBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var state = 2 # {0: up, 1: center, 2: down}
+var dir = 1 # {0: top-down, 1: down-top}
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("action"):
+		press()
+	
+func press():
+	if dir:
+		if state == 2:
+			$SwitchDown.hide()
+			state = 1
+		elif state == 1:
+			$SwitchCenter.hide()
+			state = 0
+			dir = 0
+	else:
+		if state == 0:
+			$SwitchCenter.show()
+			state = 1
+		elif state == 1:
+			$SwitchDown.show()
+			state = 2
+			dir = 1
