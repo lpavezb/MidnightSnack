5bc9785cbad581b5a022f4a503a799d0bb5ef15a
Lukas Pavez <lukas.pavez@ug.uchile.cl>
2020-11-04 16:44:49 -0300

    remove automatic level generation

--- game/src/Game.gd
+++ game/src/Game.gd
@@ -9,14 +9,15 @@ onready var robot = $Robot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	create_chunk(ground_size.x, ground_size.y)
+	pass
+	#create_chunk(ground_size.x, ground_size.y)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta):
-	var next_trigger = (next_chunk_coord.z * 10) - (ground_size.y * 10)
-	if robot.translation.z > next_trigger:
-		create_chunk(ground_size.x, ground_size.y)
+#func _physics_process(_delta):
+#	var next_trigger = (next_chunk_coord.z * 10) - (ground_size.y * 10)
+#	if robot.translation.z > next_trigger:
+#		create_chunk(ground_size.x, ground_size.y)
 
 func create_chunk(w, h):
 	var block_num
