45b8b809e391c97fb5588c83e40da957fd2e26bc
Marmando23 <71888531+Marmando23@users.noreply.github.com>
2020-12-25 22:06:48 -0300

    Barra de sueño

---
+++ game/scenes/Bar.gd
@@ -0,0 +1,12 @@
+extends HBoxContainer
+
+onready var sleepiness_bar = get_node("/root/Game/Character")
+
+func change_sleepiness(sleepiness):
+	print(sleepiness)
+	$TextureProgress.value=sleepiness
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	sleepiness_bar.connect("sleepiness_bar",self,"change_sleepiness")
