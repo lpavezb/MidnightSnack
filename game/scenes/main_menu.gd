d6c336486a512ba8006056814e369b946e0a69d3
JulianFC <julianfc1996@gmail.com>
2020-12-30 16:58:19 -0300

    Add Audio player on menu

--- game/scenes/main_menu.gd
+++ game/scenes/main_menu.gd
@@ -1,8 +1,10 @@
 extends Node
 
-
+var audio
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	audio = get_node("/root/Main menu/AudioStreamPlayer")
+	$AudioStreamPlayer.play()
 	pass
 	
 
